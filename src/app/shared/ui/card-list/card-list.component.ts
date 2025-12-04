dadaa152c2734cabbda5930eebf4bd8a69171db2
Tsimafei <xsetiqq@gmail.com>
2025-12-04 19:01:23 +0100

    fix: styles

--- src/app/shared/ui/card-list/card-list.component.ts
+++ src/app/shared/ui/card-list/card-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Card } from '../../../models/card.model';
 
 @Component({
   selector: 'app-card-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss',
+  host: { class: 'card-list' },
 })
 export class CardListComponent {
-
+  @Input()
+  cards: Card[] = [];
 }
--- src/app/shared/ui/tab-switcher/tab-switcher.component.ts
+++ src/app/shared/ui/tab-switcher/tab-switcher.component.ts
@@ -1,15 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-
 import { Observable } from 'rxjs';
-
 import { CommonModule } from '@angular/common';
 import { DashboardService } from '../../../core/services/dashboard.service';
 import { DashboardTab } from '../../../models/dashboard.model';
+import { CardListComponent } from "../card-list/card-list.component";
 
 @Component({
   selector: 'app-tab-switcher',
-  imports: [MatTabsModule, CommonModule],
+  imports: [MatTabsModule, CommonModule, CardListComponent],
   templateUrl: './tab-switcher.component.html',
   styleUrl: './tab-switcher.component.scss',
 })
